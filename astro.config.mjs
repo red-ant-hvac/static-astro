b7372bb76813500bb4065ca5609d13f466c658fe
mj-on-ubtpen-vm <mjia@ualberta.ca>
2023-04-21 17:33:55 +0000

    start with astroship theme

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,17 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+import image from "@astrojs/image";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://astroship.web3templates.com",
+  integrations: [
+    tailwind(),
+    image({
+      serviceEntryPoint: "@astrojs/image/sharp",
+    }),
+    mdx(),
+    sitemap(),
+  ],
+});
