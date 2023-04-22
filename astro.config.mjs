66edee38d8f891107143e9fd47a5c79accd0cd51
mj-on-ubtpen-vm <mjia@ualberta.ca>
2023-04-22 16:25:05 +0000

    add imageTools (optimized for lazyloading stuff)
    But not yet implemented....
    
    Again, first priority is to "get the shit to work", then worry about
    optimization and stuff

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,19 +5,13 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import yaml from "@rollup/plugin-yaml";
 
+// https://astro.build/config
 export default defineConfig({
-    vite: {
-      plugins: [
-        yaml(),
-      ],
-    },
-    site: "https://astroship.web3templates.com",
-    integrations: [
-    tailwind(),
-    image({
-      serviceEntryPoint: "@astrojs/image/sharp",
-    }),
-    mdx(),
-    sitemap(),
-  ],
+  vite: {
+    plugins: [yaml()]
+  },
+  site: "https://astroship.web3templates.com",
+  integrations: [tailwind(), image({
+    serviceEntryPoint: "@astrojs/image/sharp"
+  }), mdx(), sitemap()]
 });
