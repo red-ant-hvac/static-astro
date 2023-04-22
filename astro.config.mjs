b0b4b976fb0c7451c2e6cc889d5e2de8c5be2f95
mj-on-ubtpen-vm <mjia@ualberta.ca>
2023-04-22 03:34:43 +0000

    add photo credits (not perfect)
    Still needs brushup on:
    
    	- Better integration with yaml
    	- Set the credits differ whenever photo changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,16 @@ import tailwind from "@astrojs/tailwind";
 import image from "@astrojs/image";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
+import yaml from "@rollup/plugin-yaml";
 
 export default defineConfig({
-  site: "https://astroship.web3templates.com",
-  integrations: [
+    vite: {
+      plugins: [
+        yaml(),
+      ],
+    },
+    site: "https://astroship.web3templates.com",
+    integrations: [
     tailwind(),
     image({
       serviceEntryPoint: "@astrojs/image/sharp",
